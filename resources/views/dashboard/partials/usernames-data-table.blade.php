41dd147249a771dc051aa1d466bbd736d4370bb4
Emmanuel Versola <versolaemmanuel@gmail.com>
2023-05-17 16:31:04 +0800

    added navigation on login/register page

--- resources/views/dashboard/partials/usernames-data-table.blade.php
+++ resources/views/dashboard/partials/usernames-data-table.blade.php
@@ -22,10 +22,10 @@
               @foreach ($listUsernamesData as $data)
                 <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
                     <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                        {{ $data->username }}
+                      {{ $data->username }}
                     </th>
                     <td class="px-6 py-4">
-                        {{ $data->age == null ? "-" : $data->age }}
+                      {{ $data->age == null ? "-" : $data->age }}
                     </td>
                     <td class="px-6 py-4">
                       {{ $data->race == null ? "-" : $data->race }}
